6a17bef4dcbf3780b7f1a1c3d26aed5000cc2df8
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-01-10 17:40:35 -0300

    Create sys_date.py

---
+++ scripts/sys_date.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+def get_date():
+  today = date.today()
+  dia_hoje = today.strftime("%d/%m/%Y")
+  dia_semana = [
+      'Segunda-feira',
+      'Terça-feira',
+      'Quarta-feira',
+      'Quinta-Feira',
+      'Sexta-feira',
+      'Sábado',
+      'Domingo'
+  ]
+  dia_da_semana = dia_semana[today.weekday()].lower()
+  data_completa = f"""{dia_da_semana}, {dia_hoje}"""
+  return data_completa
