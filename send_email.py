eb1d31a77c942246baec9d2b9aaf414e51cea124
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-01-12 14:56:07 -0300

    melhorar distribuicao de img capa impressa

--- send_email.py
+++ send_email.py
@@ -15,6 +15,7 @@ manchete_oglobo = link_oglobo()
 manchete_metropoles = link_metropoles()
 data_completa = get_date()
 impressa_links = capa_jornal()
+
 email = Mail(
       from_email = "gabriela.caesar.2019@gmail.com", 
       to_emails = ["gabriela.caesar.g1@gmail.com"], 
@@ -23,19 +24,22 @@ email = Mail(
       <h2>Projeto final de <a href='http://gabrielacaesar.com/' target='_blank'>Gabriela Caesar</a></h2>
       <h3>Este projeto apoia o jornalismo profissional. O acesso a informações muda vidas. Ajude a imprensa. Assine jornais, sites e revistas. Apoie grandes e pequenos veículos. Busque informações antes de opinar. Cheque informações antes de compartilhar.</h3>
       <h4>(versão 1.0)</h4>
+      <h3>Manchete dos principais portais</h3>
       <p>Globo.com: {manchete_globo_com}</p>
       <p>g1: {manchete_g1}</p>
-      <p>Valor: {manchete_valor}</p>
-      <p>UOL: {manchete_uol}</p>
-      <p>Folha: {manchete_folha}</p>
-      <p>Estadão: {manchete_estadao}</p>
-      <p>O Globo: {manchete_oglobo}</p>
-      <p>Metrópoles: {manchete_metropoles}</p>
-      <h4>Capa das versões impressas</h4>
-      <img src={impressa_links[0]}>
-      <img src={impressa_links[1]}>
-      <img src={impressa_links[2]}>
-      <img src={impressa_links[3]}>
+	<p>Valor: {manchete_valor}</p>
+	<p>UOL: {manchete_uol}</p>
+	<p>Folha: {manchete_folha}</p>
+	<p>Estadão: {manchete_estadao}</p>
+	<p>O Globo: {manchete_oglobo}</p>
+	<p>Metrópoles: {manchete_metropoles}</p>
+      </br>
+      </br>
+      <h3>Capa das versões impressas</h3>
+      <p>Folha:</p><img src={impressa_links[0]}></br></br></br></br>
+      <p>Estadão:</p><img src={impressa_links[1]}></br></br></br></br>
+      <p>O Globo:</p><img src={impressa_links[2]}></br></br></br></br>
+      <p>Valor:</p><img src={impressa_links[3]}>
       """
       )
 
