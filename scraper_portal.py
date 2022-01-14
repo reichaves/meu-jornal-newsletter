3243b924f45e5b6026a1a69765a87f1a3db61e4f
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-01-14 12:33:14 -0300

    correction metropoles

--- scraper_portal.py
+++ scraper_portal.py
@@ -62,3 +62,6 @@ def link_oglobo():
 def link_metropoles():
   url = "https://www.metropoles.com/"
   page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.27 Safari/537.36"})
+  soup = BeautifulSoup(page.content, "html.parser")
+  manchete_metropoles = soup.find('h2', class_ = 'm-title').find('a').attrs['href']
+  return manchete_metropoles
